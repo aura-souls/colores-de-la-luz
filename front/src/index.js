79fb02682faaa7c7e6e4c11a90d85cdc88cd8b8b
Margarita Naluanga Mbande Mandje <131756045+Rita7979@users.noreply.github.com>
2023-09-18 17:18:34 +0200

    Media Card

--- front/src/index.js
+++ front/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CardMedia from './components/card/MediaCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    < CardMedia />
   </React.StrictMode>
 );
 
