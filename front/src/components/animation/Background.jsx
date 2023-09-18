31253e70aaa2a4969381b14f021b0aeede9a068a
LucíaSilva <coderalsc@gmail.com>
2023-09-19 01:56:59 +0200

    animation mode

---
+++ front/src/components/animation/Background.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './background.css';
+
+function Background() {
+      return (
+
+            <div className="ripple-background">
+              <div className="circle xxlarge shade1"></div>
+              <div className="circle xlarge shade2"></div>
+              <div className="circle large shade3"></div>
+              <div className="circle medium shade4"></div>
+              <div className="circle small shade5"></div>
+            </div>
+          );
+        }
+
+        
+
+export default Background;
--- front/src/pages/Contact.jsx
+++ front/src/pages/Contact.jsx
@@ -4,7 +4,7 @@ import {React} from 'react'
 function Contact() {
   return (
     <div>
-      <Navbar />
+    
 
     </div>
   )
