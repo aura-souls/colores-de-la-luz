3fbf5410cdf4f7f17a7b5af5b4876402861f155e
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-09-18 22:18:04 +0200

    Creating the user logout function in the front section, and configuration in cors and .env file in the back section

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import FormSignin from '../components/forms/formSignin/FormSignin'
 import FormLogin from '../components/forms/formLogin/FormLogin'
+import { AuthService } from '../services/AuthServices'
 
 function Main() {
+
+  const auth = AuthService();
+
+  const handleLogout = (e) =>{
+    e.preventDefault();
+
+    auth.logout().then(res => {
+      localStorage.removeItem('auth_token');
+      alert(res.data.msg);
+    }).catch(error => console.log(error));
+}
+  
   return (
     <div>
         <FormSignin/>
         <FormLogin/>
+        <button onClick={handleLogout}>Logout</button>
     </div>
   )
 }
