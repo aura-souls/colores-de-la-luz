1f149b5ef7a4d2a0b1904a16c165bb427503dadd
LucíaSilva <coderalsc@gmail.com>
2023-09-19 00:40:49 +0200

    navbar y footer functional and responsive

--- front/src/components/footer/Footer.jsx
+++ front/src/components/footer/Footer.jsx
@@ -5,6 +5,7 @@ import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import { Instagram, WhatsApp } from "@mui/icons-material";
 import { Box } from "@mui/material";
+import Hidden from "@mui/material/Hidden";
 
 export default function Footer() {
   return (
@@ -18,7 +19,6 @@ export default function Footer() {
         bottom: 0,
         left: 0,
         right: 0,
-        height: "10px",
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
@@ -26,30 +26,43 @@ export default function Footer() {
     >
       <Container maxWidth="lg">
         <Grid container spacing={2} alignItems="center">
-          <Grid item xs={12} sm={8} sx={{ textAlign: "left" }}>
+          <Grid item xs={12} sm={8}>
+            <Hidden mdUp>
+              <div style={{ display: "flex", justifyContent: "center" }}>
                 <Typography variant="body2" color="inherit">
-              {"Copyright © "}
-              <Link color="inherit" href="https://coloresdelmundo.com">
-                Colores del mundo
-              </Link>{" "}
-              {new Date().getFullYear()}
-              {"."} <br></br>
-              {"Política de privacidad"}
-
-            </Typography>
+                  {"Copyright ©2023 "}
+                </Typography>
+              </div>
+            </Hidden>
+            <Hidden smDown>
+              <Typography variant="body2" color="inherit">
+                {"Copyright ©2023 "}
+              </Typography>
+            </Hidden>
           </Grid>
-          <Grid item xs={12} sm={4} sx={{ display: "flex", justifyContent: "flex-end", textAlign: "right" }}>
-            {/* Redes Sociales a la derecha */}
-            <Link
-              href="https://www.whatsapp.com"
-              color="inherit"
-              sx={{ pl: 1, pr: 1 }}
-            >
-              <Instagram />
-            </Link>
-            <Link href="https://www.whatsapp.com" color="inherit">
-              <WhatsApp />
-            </Link>
+          <Grid item xs={12} sm={4}>
+            {/* Redes Sociales a la derecha en la versión de escritorio */}
+            <Hidden smDown>
+              <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                <Link href="https://www.instagram.com" color="inherit" sx={{ pl: 1, pr: 1 }}>
+                  <Instagram />
+                </Link>
+                <Link href="https://www.whatsapp.com" color="inherit">
+                  <WhatsApp />
+                </Link>
+              </div>
+            </Hidden>
+            {/* Redes Sociales debajo en la versión móvil */}
+            <Hidden mdUp>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Link href="https://www.instagram.com" color="inherit" sx={{ pl: 1, pr: 1 }}>
+                  <Instagram />
+                </Link>
+                <Link href="https://www.whatsapp.com" color="inherit">
+                  <WhatsApp />
+                </Link>
+              </div>
+            </Hidden>
           </Grid>
         </Grid>
       </Container>
--- front/src/pages/Contact.jsx
+++ front/src/pages/Contact.jsx
@@ -1,10 +1,10 @@
 import {React} from 'react'
-import Form from '../components/form/Form';
+
 
 function Contact() {
   return (
     <div>
-       
+
     </div>
   )
 }
