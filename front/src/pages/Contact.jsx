44d3bf8476e542e0df8795b779d94aa40ce61c90
Teresa1965 <132339878+Teresa1965@users.noreply.github.com>
2023-09-20 12:33:41 +0200

    forulrio prueba 1

---
+++ front/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import {React} from 'react'
+import ContactForm from '../components/form/contactform/ContactForm'
+
+function ContactForm() {
+  return (
+    <div>
+       <ContactForm />
+    </div>
+  )
+}
+
+export default ContactForm
