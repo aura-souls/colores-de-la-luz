706dda227b66cfb80f31b99bb3b08c8794cc2795
LucíaSilva <coderalsc@gmail.com>
2023-09-20 10:53:55 +0200

    navbar and footer without animation

--- front/src/pages/AboutMe.jsx
+++ front/src/pages/AboutMe.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
-import Background from "../components/animation/Background";
+
 
 
 function Home() {
   return (
     <div>
         <Navbar />
-        <Background />
+
         <Footer />
     </div>
   )
