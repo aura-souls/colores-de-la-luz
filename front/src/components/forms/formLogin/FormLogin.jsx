a5993d2cbabfdf9d0574f145814224a76577c635
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-09-20 04:32:01 +0200

    Styling the user login form in the front section

--- front/src/components/forms/formLogin/FormLogin.jsx
+++ front/src/components/forms/formLogin/FormLogin.jsx
@@ -1,5 +1,18 @@
 import React, { useState } from 'react'
 import { AuthService } from '../../../services/AuthServices';
+import { Grid, TextField, Button, Card, CardContent, Typography, Dialog, DialogTitle, DialogContent, DialogActions, createTheme, ThemeProvider } from '@mui/material';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+
+const theme = createTheme({
+    palette: {
+        inputborder: {
+            main: '#4A148C',
+        },
+        buttoncolor: {
+            main: '#512872',
+        },
+    }
+}) 
 
 function FormLogin() {
 
@@ -7,6 +20,8 @@ function FormLogin() {
         email: '',
         password: ''
     })
+    const [alert, setAlert] = useState(''); 
+    const [openAlert, setOpenAlert] = useState(false); 
     
     const auth = AuthService();
 
@@ -19,26 +34,87 @@ function FormLogin() {
     const handleOnSubmit = (e) => {
         e.preventDefault();
         auth.login(login).then(res => {
-            console.log(res.data.token)
             localStorage.setItem('auth_token', res.data.token);
-            alert(res.data.msg);
+            setAlert(res.data.msg);
+            setOpenAlert(true);
+            setLogin({
+                name: '',
+                email: '',
+                password: ''
+            });
         }).catch(error => console.log(error));
+    };
+
+    const handleCloseAlert = () => {
+        setOpenAlert(false);
     };
 
   return (
     <div>
-        <h2>Login de usuario</h2>
-        <form onSubmit={handleOnSubmit}>
-            <div>
-                <label htmlFor='email'>Email</label>
-                <input type="email" name='email' onChange={handleOnChange} value={login.email}/>
-            </div>
-            <div>
-                <label htmlFor='password'>Password</label>
-                <input type="password" name='password' onChange={handleOnChange} value={login.password}/>
-            </div>
-            <button type='submit'>Enviar</button>
-        </form>
+        <ThemeProvider theme={theme}>
+            <Grid>
+                <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" style={{ textAlign: 'center', color: '#4A148C' }}>ACCESO DE USUARIO</Typography> 
+
+                        <Typography variant="body2" color="textSecondary" component="p" gutterBottom>*** Este formulario solo está disponible para el administrador</Typography>
+
+                        <form onSubmit={handleOnSubmit}>
+                            <Grid container spacing={1}>
+                                <Grid item xs={12}>
+                                    <TextField 
+                                    type="email" 
+                                    placeholder="Introduzca un email" 
+                                    label="Correo Electrónico" 
+                                    variant="outlined" 
+                                    fullWidth 
+                                    required 
+                                    name='email' 
+                                    onChange={handleOnChange} 
+                                    value={login.email} 
+                                    color='inputborder'
+                                    InputLabelProps={{
+                                        style: { color: '#4A148C' } 
+                                    }}/>
+                                </Grid>
+                                
+                                <Grid item xs={12}>
+                                    <TextField 
+                                    type="password" 
+                                    placeholder="Introduzca una contraseña" 
+                                    label="Contraseña" 
+                                    variant="outlined" 
+                                    fullWidth 
+                                    required 
+                                    name='password' 
+                                    onChange={handleOnChange} 
+                                    value={login.password}
+                                    color='inputborder'
+                                    InputLabelProps={{
+                                        style: { color: '#4A148C' } 
+                                    }}/>
+                                </Grid>
+
+                                <Grid item xs={12}>
+                                    <Button type="submit" variant="contained" color="buttoncolor" fullWidth style={{ color: 'white' }}>Enviar</Button>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </CardContent>
+                </Card>
+            </Grid>
+        
+            <Dialog open={openAlert} onClose={handleCloseAlert} > 
+                <DialogContent>
+                    <DialogTitle style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}><CheckCircleOutlineIcon color='success' style={{ marginRight: '0.2rem' }}/>Logrado</DialogTitle>
+                    <Typography variant="body1">{alert}</Typography>
+                </DialogContent>
+
+                    <DialogActions>
+                        <Button onClick={handleCloseAlert} color="primary" autoFocus>OK</Button>
+                    </DialogActions>
+            </Dialog>
+        </ThemeProvider>    
     </div>
   )
 }
