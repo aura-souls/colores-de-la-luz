47e450ed56020435d4034ca2c92ae127904efb1a
Margarita Naluanga Mbande Mandje <131756045+Rita7979@users.noreply.github.com>
2023-09-22 19:11:50 +0200

    chackra/points/home

---
+++ front/src/components/chakra/Chackra.jsx
@@ -0,0 +1,38 @@
+import Grid from '@mui/material/Grid';
+import React from 'react';
+
+
+function Chackra() {
+  return (
+    <div>
+      <Grid container spacing={2}>
+  <Grid item xs={2}>
+    <img src="assets/imges/foto1.svg" alt="Descripción de la imagen 1" />
+  </Grid>
+  <Grid item xs={2}>
+    <img src="assets/imges/foto2.svg" alt="Descripción de la imagen 2" />
+  </Grid>
+  <Grid item xs={2}>
+    <img src="assets/imges/foto3.svg" alt="Descripción de la imagen 3" />
+  </Grid>
+  <Grid item xs={2}>
+    <img src="assets/imges/foto4.svg" alt="Descripción de la imagen 4" />
+  </Grid>
+  <Grid item xs={2}>
+    <img src="assets/imges/foto5.svg" alt="Descripción de la imagen 5" />
+  </Grid>
+  <Grid item xs={2}>
+    <img src="assets/imges/foto6.svg" alt="Descripción de la imagen 6" />
+  </Grid>
+  <Grid item xs={2}>
+    <img src="assets/imges/foto7.svg" alt="Descripción de la imagen 7" />
+  </Grid>
+</Grid>
+
+    </div>
+  )
+}
+
+export default Chackra
+
+
