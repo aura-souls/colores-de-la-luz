5318840fceddb9c0eadd4d93e8e4c6f2f1ebe61a
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-09-24 03:04:38 +0200

    Correction on the AboutMe file

--- front/src/pages/AboutMe.jsx
+++ front/src/pages/AboutMe.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import Navbar from "../components/navbar/Navbar";
-import Footer from "../components/footer/Footer";
 
-
-
-function Home() {
+function AboutMe() {
   return (
     <div>
-        <Navbar />
-
-        <Footer />
     </div>
   )
 }
 
-export default Home
+export default AboutMe
