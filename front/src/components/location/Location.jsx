89df375bc85cb707e03c2822bc1b428f8d33dd9a
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-09-27 03:07:07 +0200

    styling Location component

--- front/src/components/location/Location.jsx
+++ front/src/components/location/Location.jsx
@@ -1,30 +1,79 @@
 import {React} from 'react'
 import AlnouartLocation from '../../assets/images/location/alnouart-location.jpg'
+import { styled } from '@mui/material/styles';
+import { Box, Grid, Typography, createTheme, ThemeProvider } from '@mui/material';
+
+const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+});
+
+const theme = createTheme({
+    typography: {
+        h6: {
+            color: '#4A148C',
+            fontWeight: 1000,
+        },
+        body2: {
+            color: '#4A148C',
+        },
+        body3: {
+            color: '#4A148C',
+            fontWeight: 700,
+            margin: '0 0 0 1rem'
+        },
+    }
+}) 
 
 function Location() {
   return (
     <div>
-        <div>
-            <div>
-                <h4>Dirección:</h4>
-                <h5>ALNOUART</h5>
-                <p>Carrer de Berga 36</p>
-                <p>C.P. 08012</p>
-                <p>Barcelona</p>
-            </div>
+        <ThemeProvider theme={theme}>
+            <Box sx={{ width: '70%', padding: '5rem 2rem', margin: '0 auto' }}>
+                <Grid container spacing={2}>   
+                    <Grid item xs={12} sm container>
+                        <Grid item xs container direction='column' spacing={2} >
+                            <Grid item xs>
+                                <Typography gutterBottom variant='h6' component='div'>Dirección:</Typography>
 
-            <div>
-                <h4>Horarios:</h4>
-                <h5>Lunes a viernes</h5><span> de 8:00 a 18:00</span>
-                <h5>Sábados de</h5><span> de 9:00 a 14:00</span>
-                <h5>Domingos cerrados</h5>
-            </div>
+                                <Typography variant='body2' gutterBottom style={{ margin: '0 1rem' }}>Carrer de Berga 36</Typography>
 
-        </div>
-        <div>
-            <h4>Localización:</h4>
-            <img src={AlnouartLocation} alt='Localización de Alnouart'/>
-        </div>
+                                <Typography variant='body2' gutterBottom style={{ margin: '0 1rem' }}>C.P. 08012</Typography>
+
+                                <Typography variant='body2' color='text.secondary' style={{ margin: '0 1rem' }}>Barcelona</Typography>
+                            </Grid>
+
+                            <Grid item xs>
+                                <Typography gutterBottom variant='h6' component='div'>Horarios:</Typography>
+                                
+                                <div>
+                                    <Typography variant='body3' gutterBottom>Lunes a viernes </Typography>
+                                    <Typography component='span' variant='body2'> de 8:00 a 18:00</Typography>
+                                </div>
+                                
+                                
+                                <div>
+                                    <Typography variant='body3' gutterBottom>Sábados</Typography>
+                                    <Typography component='span' variant='body2'> de 9:00 a 14:00</Typography>
+                                </div>
+
+                                <Typography variant='body3'>Domingos cerrados</Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    
+                    <Grid item XS={6} sm container>
+                        <Grid item xs>
+                            <Typography gutterBottom variant='h6' component='div' textAlign='center'>Localización:</Typography>
+                        </Grid>
+                        
+                        <Img src={AlnouartLocation} alt='Localización de Alnouart' style={{ borderRadius: '1rem', border: '0.1rem solid rgba(74, 20, 140, 0.3' }}/>
+                    </Grid>
+                </Grid>
+            </Box>
+        </ThemeProvider>
     </div>
   )
 }
