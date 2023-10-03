57eefb4222d4d812046e805e27165549c4a57330
Ana Lucía Silva Córdoba <131855670+alusilco@users.noreply.github.com>
2023-10-03 12:27:44 +0200

    whatsapp back working

--- back/routes/web.php
+++ back/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MailController;
+use App\Http\Controllers\WhatsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
