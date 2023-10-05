670a6e21743017dbc09f201a0fa3a6c9853b3259
Ana Lucía Silva Córdoba <131855670+alusilco@users.noreply.github.com>
2023-10-05 09:46:57 +0200

    adding whatsapp service

--- back/routes/api.php
+++ back/routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WhatsController;
+
 
 /*
 |--------------------------------------------------------------------------
