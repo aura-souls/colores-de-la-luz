14b3af3de325f417ee7c0e714b90c4c263740e1a
Ana Lucía Silva Córdoba <131855670+alusilco@users.noreply.github.com>
2023-10-08 19:10:20 +0200

    color chacras change

--- front/src/components/introHome/IntroHome.jsx
+++ front/src/components/introHome/IntroHome.jsx
@@ -5,11 +5,11 @@ import './intro-home.css';
 
 function IntroHome() {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 8, mb: 3, color:'#4A148C' }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4, mb: 2, color:'#4A148C' }}>
       <Typography variant="h3" align="center" mb="1.50rem" ml="1.7rem" mr="1.7rem" >
         Los colores de la luz
       </Typography>
-      <Typography variant="body1" align="center"mt="2rem" mb="1.50rem" ml="1.7rem" mr="1.7rem" >
+      <Typography variant="body1" align="center" mt="-1rem" mb="-2rem" ml="1.7rem" mr="1.7rem" >
         En el Cuerpo Humano, tienes unos centros energéticos,{<br />} llamados chakras  donde es importantísimo mantenerlos en armonía ya que si están bloqueados nuestra vida también lo estará.{<br />}
         Cada centro de energía tiene un color y un poder. Empieza todo en el chakra rojo posicionado en el sacro y acaba en el violeta posicionado en la cabeza.
       </Typography>
@@ -18,4 +18,5 @@ function IntroHome() {
 }
 
 export default IntroHome;
+
 
--- front/src/components/pointsOrganism/PointsComponent.jsx
+++ front/src/components/pointsOrganism/PointsComponent.jsx
@@ -11,20 +11,20 @@ import red from '../../assets/images/red.png';
 import './points.css';
 
 const Chackra = () => {
-  const chakrasData = [ 
-    { imageSrc: red, text: 'Muladhara “Yo Tengo” si no está equilibrado es El Miedo.', backgroundColor: 'rgba(241, 79, 68, 0.15)' }, 
-    { imageSrc: orange, text: 'Svadhishthana “Yo Acciono, disfruto y creo, si no está equilibrado es por la creencia de la Culpa.', backgroundColor: 'rgba(250, 168, 75, 0.15)'},
-    { imageSrc: yellow, text: 'Manipura “Yo Puedo”, si no está equilibrado sentimos Vergüenza.', backgroundColor: 'rgba(246, 218, 75, 0.15)'},
-    { imageSrc: green, text: 'Anahata “Yo Amo”, si no está equilibrado sentimos Tristeza.', backgroundColor: 'rgba(160, 209, 102, 0.15)'},
-    { imageSrc: celeste, text: 'Vishuddha“ Yo Expreso”si no está equilibrado nos Mentimos.', backgroundColor: 'rgba(50, 202, 235, 0.15)'},
-    { imageSrc: blue, text: 'Ajna “Yo intuyo” si no está equilibrado nos estamos negando.', backgroundColor: 'rgba(54, 139, 193, 0.15)' },
-    { imageSrc: violet, text: 'Sahastrara “Yo Soy” si no está equilibrado estamos viviendo en el Control Mental', backgroundColor: 'rgba(203, 169, 205, 0.15)' }, 
-    
+  const chakrasData = [
+    { imageSrc: red, text: 'Muladhara “Yo Tengo” si no está equilibrado es El Miedo.', backgroundColor: 'rgba(241, 79, 68, 0.3)' },
+    { imageSrc: orange, text: 'Svadhishthana “Yo Acciono, disfruto y creo, si no está equilibrado es por la creencia de la Culpa.', backgroundColor: 'rgba(250, 168, 75, 0.4)'},
+    { imageSrc: yellow, text: 'Manipura “Yo Puedo”, si no está equilibrado sentimos Vergüenza.', backgroundColor: 'rgba(246, 218, 75, 0.7)'},
+    { imageSrc: green, text: 'Anahata “Yo Amo”, si no está equilibrado sentimos Tristeza.', backgroundColor: 'rgba(176, 242, 180, 0.7)'},
+    { imageSrc: celeste, text: 'Vishuddha“ Yo Expreso”si no está equilibrado nos Mentimos.', backgroundColor: 'rgba(50, 202, 235, 0.2)'},
+    { imageSrc: blue, text: 'Ajna “Yo intuyo” si no está equilibrado nos estamos negando.', backgroundColor: 'rgba(54, 139, 193, 0.5)' },
+    { imageSrc: violet, text: 'Sahastrara “Yo Soy” si no está equilibrado estamos viviendo en el Control Mental', backgroundColor: 'rgba(203, 169, 205, 0.45)' },
+
   ];
-  
+
   const [selectedChakra, setSelectedChakra] = useState(null);
   const [showText, setShowText] = useState(false);
-  
+
   const handleClick = (index,event) => {
     event.stopPropagation();
     if (selectedChakra === index) {
@@ -35,16 +35,16 @@ const Chackra = () => {
       setShowText(true);
     }
   }
-  
+
   return (
-    <div className="chakra-grid" style={{ display: 'flex', justifyContent: 'center', margin:'5rem'}}>
+    <div className="chakra-grid" style={{ display: 'flex', justifyContent: 'center', margin:'3rem'}}>
       {chakrasData.map((data, index) => (
         <div key={index} onClick={(event) => handleClick(index,event)} style={{ textAlign: 'center',}}>
-          {(selectedChakra === null || selectedChakra === index) && <img src={data.imageSrc} alt="Chakra images" style={{marginLeft:14, marginRight:14, marginTop:10, width: 100, height:100,boxShadow: '1px 3px 4px gray', borderRadius:'50px'}} />}
+          {(selectedChakra === null || selectedChakra === index) && <img src={data.imageSrc} alt="Chakra images" style={{marginLeft:14, marginRight:14, width: 100, height:100,boxShadow: '1px 3px 4px gray', borderRadius:'50px'}} />}
           {(selectedChakra === null || selectedChakra === index) && showText &&
-            <Card variant="outlined" sx={{ backgroundColor: data.backgroundColor , borderRadius:6 , marginTop:6, height:180, overflowY:'no-scroll',}} className="fade-in">
+            <Card variant="outlined" sx={{ backgroundColor: data.backgroundColor , borderRadius:6 , marginTop:3, height:180, overflowY:'no-scroll',}} className="fade-in">
               <CardContent>
-                <Typography sx={{ color:'#6A1B9A', pt:'1.25rem',}}>{data.text}</Typography>
+                <Typography sx={{ color:'#6A1B9A', pt:'3rem',}}>{data.text}</Typography>
               </CardContent>
             </Card>
           }
