fbb04dba311c45597ca46f485c06a7f9a3d237f1
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-10-09 07:08:34 +0200

    modif styling and some words, and deleting the TherapyMediaCard

--- front/src/components/therapies/Therapy.jsx
+++ front/src/components/therapies/Therapy.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { TherapiesService } from '../../services/TherapiesService'
 import TherapyCard from './therapyCard/TherapyCard';
 import TherapyTitle from './therapyTitle/TherapyTitle';
-import TherapyMediaCard from './therapyMediaCard/TherapyMediaCard'
-import { Grid } from '@mui/material';
 
 function Therapy() {
 
@@ -18,15 +16,7 @@ function Therapy() {
    
   return (
     <div>
-      <TherapyTitle color='#6A1B9A' align="center" texto="Nuestras Terapias"/>
-
-      <Grid container  sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 'auto', flexDirection: 'row' }}>
-        {
-          therapies.map((therapy, index) => (
-            <TherapyMediaCard key={index} therapy={therapy}/>
-          ))
-        }
-      </Grid>
+      <TherapyTitle color='#6A1B9A' align="center" texto="Terapias"/>
       
       <div style={{ margin: '0 2rem'}}>
         {
--- front/src/components/therapies/therapyTitle/TherapyTitle.jsx
+++ front/src/components/therapies/therapyTitle/TherapyTitle.jsx
@@ -5,9 +5,7 @@ import Box from '@mui/material/Box';
 function TherapyTitle() {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 6, mb: 3, color:'#4A148C' }}>
-      <Typography variant="h3" align="center">
-        Nuestras terapias
-      </Typography>
+      <Typography variant="h3" align="center">Terapias</Typography>
     </Box>
   );
 }
