515578bf612244e475f59021644c01faca97f0fc
JacinemiUA <131624010+JacinemiUA@users.noreply.github.com>
2023-10-09 06:50:52 +0200

    styling Admin Dashboard in the front section

--- front/src/components/adminDashboard/AdminDashboard.jsx
+++ front/src/components/adminDashboard/AdminDashboard.jsx
@@ -2,6 +2,23 @@ import React, { useEffect, useState } from "react";
 import { TherapiesService } from "../../services/TherapiesService";
 import AdminTable from "./AdminTable";
 import { Link } from 'react-router-dom';
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material";
+import AddToPhotosIcon from "@mui/icons-material/AddToPhotos";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+}));
   
 function AdminDashboard() {
     const [therapies, setTherapies] = useState([]);
@@ -16,24 +33,28 @@ function AdminDashboard() {
 
   return (
     <div style={{ margin: "2rem" }}>
-        <Link to="/crear-una-nueva-terapia"><button>Agregar</button></Link>
-        <table>
-            <tbody>
-                <tr>
-                    <th>Id</th>
-                    <th>Nombre</th>
-                    <th>Imagen</th>  
-                    <th>Descripción</th>
-                    <th>Editar/Borrar</th> 
-                </tr>
+        <Link to="/crear-una-nueva-terapia"><Button variant="contained" startIcon={<AddToPhotosIcon/>} style={{ fontSize: 12, background: "#4A148C", borderRadius: "0.5rem 0.5rem 0 0", marginLeft: "0.15rem" }}>Agregar</Button></Link>
+    
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>    
+                    <TableRow>
+                        <StyledTableCell align="center">Id</StyledTableCell>
+                        <StyledTableCell align="center">Nombre</StyledTableCell>
+                        <StyledTableCell align="center">Imagen</StyledTableCell>
+                        <StyledTableCell align="center">Descripción</StyledTableCell>
+                        <StyledTableCell align="center">Editar/Borrar</StyledTableCell>
+                    </TableRow>
+                </TableHead>
 
-                {
-                    therapies.map((therapy, index) => (
-                        <AdminTable key={index} therapy={therapy}/>
-                    ))
-                }              
-            </tbody>
-        </table> 
+                <TableBody>
+                    {
+                        therapies.map((therapy, index) => (
+                            <AdminTable key={index} therapy={therapy}/>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     </div>
   )
 }
--- front/src/components/adminDashboard/AdminTable.jsx
+++ front/src/components/adminDashboard/AdminTable.jsx
@@ -5,10 +5,6 @@ import TableRow from "@mui/material/TableRow";
 import Button from "@mui/material/Button";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { TherapiesService } from "../../services/TherapiesService";
-import { Dialog, DialogActions, DialogContent, DialogTitle, Typography } from "@mui/material";
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import { Link } from "react-router-dom";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.body}`]: {
@@ -27,47 +23,33 @@ const StyledTableRow = styled(TableRow)({
 });
 
 function AdminTable({ therapy }) {
-  const api = TherapiesService();
-  const [error, setError] = useState(null);
-  const [alert, setAlert] = useState(''); 
-  const [openAlert, setOpenAlert] = useState(false);
-
-  const handleDelete = (id) => {
-    api.deleteTherapy(id).then((res) => {
-        setAlert(res.data.msg);
-        setOpenAlert(true);
-        setTimeout(() => {
-          window.location.reload();
-        }, 2000);
-      }).catch((error) => {
-        setError("Error al eliminar la terapia:", error);
-      });
-  };
-
-  const handleCloseAlert = () => {
-    setOpenAlert(false);
-  };
-
   return (
     <>
-      <tr>
-        <td>{therapy.id}</td>
-        <td>{therapy.name}</td>
-        <td>
+      <StyledTableRow>
+        <StyledTableCell component="th" scope="row" align="center">{therapy.id}</StyledTableCell>
+
+        <StyledTableCell align="center">{therapy.name}</StyledTableCell>
+
+        <StyledTableCell align="center">
           {isURL(therapy.image) ? (
             <img src={therapy.image} alt={therapy.name} style={{ width: "5rem" }} />
           ) : (
             <img src={`http://127.0.0.1:8000/storage/${therapy.image}`} alt={therapy.name} style={{ width: "5rem" }} />
           )}
-        </td>  
-        <td>{therapy.description}</td>
-        <td>
-            <button>Editar</button>
-            <button>Borrar</button>
-        </td> 
-      </tr> 
-      
-      {error && <div className="error-message">{error}</div>}
+        </StyledTableCell>
+
+        <StyledTableCell align="center">{therapy.description}</StyledTableCell>
+
+        <StyledTableCell align="center">
+          <div>
+            <Button variant="contained" startIcon={<ModeEditIcon />} style={{ fontSize: 12, width: "7rem", marginBottom: "0.5rem", background: "#4A148C" }}>Editar</Button>
+          </div>
+
+          <div>
+            <Button variant="contained" startIcon={<DeleteIcon />} style={{ fontSize: 12, width: "7rem", background: "#4A148C" }}>Borrar</Button>
+          </div>
+        </StyledTableCell>
+      </StyledTableRow>
     </>
   );
 }
